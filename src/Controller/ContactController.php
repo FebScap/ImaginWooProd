790572fe2b80c1227b7d404040c3ea2c15f19c15
Nicolas Vanhorle <vanhorlenicolas@gmail.com>
2024-09-05 12:11:18 +0200

    fix(content): modifications avec client sur demande

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,15 +4,34 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    /**
+     * @throws TransportExceptionInterface
+     */
+    #[Route('/contact/', name: 'app_contact')]
+    public function sendEmail(MailerInterface $mailer): Response
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-        ]);
+        $email = (new TemplatedEmail())
+            ->from('clubphoto@casea.be')
+            ->to('vanhorlenicolas@gmail.com')
+            ->subject('Thanks for signing up!')
+
+            // path of the Twig template to render
+            ->htmlTemplate('contact/email.html.twig')
+
+            // pass variables (name => value) to the template
+            ->context([
+                'expiration_date' => new \DateTime('+7 days'),
+                'username' => 'foo',
+            ])
+        ;
+        $mailer->send($email);
     }
 }
