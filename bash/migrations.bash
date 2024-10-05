c4d10c211969da5c7912ae6c5dff8f87e9654c3e
Nicolas Vanhorle <vanhorlenicolas@gmail.com>
2024-10-05 17:27:54 +0200

    fix(migrations.bash): fix de migrations.bash pour le repo distant

--- bash/migrations.bash
+++ bash/migrations.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Ce fichier permet de drop et rebase la DB rapidement (à partir des fichier '/migrations/sql-dump.sql' et '/migrations/VersionYYYYMMDDHHMMSS'
 echo "Drop de la DB :"
-symfony.exe console doctrine:migrations:execute DoctrineMigrations\\Version20241005103343 --down --no-interaction
+php bin/console doctrine:migrations:execute DoctrineMigrations\\Version20241005103343 --down --no-interaction
 
 echo "Up de la DB :"
-symfony.exe console doctrine:migrations:execute DoctrineMigrations\\Version20241005103343 --up --no-interaction
+php bin/console doctrine:migrations:execute DoctrineMigrations\\Version20241005103343 --up --no-interaction
